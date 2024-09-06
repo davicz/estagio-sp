7fa3e1f898dd7235d3e5528f4e523f5e81fe2028
davi <davicezarpe@gmail.com>
2024-09-06 00:45:07 -0300

    first commit

---
+++ src/application/ProgramAtividade2.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Scanner;
+
+public class ProgramAtividade2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Informe um número: ");
+		int num = scanner.nextInt();
+
+		if (pertenceFibonacci(num)) {
+			System.out.println(num + " pertence à sequência de Fibonacci.");
+		} else {
+			System.out.println(num + " não pertence à sequência de Fibonacci.");
+		}
+
+		scanner.close();
+	}
+
+	public static boolean pertenceFibonacci(int num) {
+		int i = 0, j = 1, k;
+
+		if (num == 0 || num == 1) {
+			return true;
+		}
+
+		while (j <= num) {
+			k = i + j;
+			i = j;
+			j = k;
+
+			if (j == num) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
